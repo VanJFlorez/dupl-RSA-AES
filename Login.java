783d5a8b05c673ea47c795caf19b2183b34b1fd7
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-09-13 15:06:51 -0500

    RSA key generation

--- Login.java
+++ Login.java
@@ -1,7 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.swing.*;
 
 public class Login extends JFrame implements ActionListener {
   private JPanel panel;
@@ -9,36 +11,38 @@ public class Login extends JFrame implements ActionListener {
   private JTextField userName_text;
   private JPasswordField password_text;
   private JButton submit, cancel;
-  
-   public Login() {
-      // Username Label
-      user_label = new JLabel();
-      user_label.setText("Usuario :");
-      userName_text = new JTextField();
-      // Password Label
-      password_label = new JLabel();
-      password_label.setText("Contrsena :");
-      password_text = new JPasswordField();
-      // Submit
-      submit = new JButton("Cifrar...");
-      panel = new JPanel(new GridLayout(3, 1));
-      panel.add(user_label);
-      panel.add(userName_text);
-      panel.add(password_label);
-      panel.add(password_text);
-      message = new JLabel();
-      panel.add(message);
-      panel.add(submit);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      // Adding the listeners to components..
-      submit.addActionListener(this);
-      add(panel, BorderLayout.CENTER);
-      setTitle("Please Login Here !");
-      setSize(450,350);
-      setVisible(true);
-   }
-   public static void main(String[] args) {
+
+  public Login() {
+    // Username Label
+    user_label = new JLabel();
+    user_label.setText("Usuario :");
+    userName_text = new JTextField();
+    // Password Label
+    password_label = new JLabel();
+    password_label.setText("Contrsena :");
+    password_text = new JPasswordField();
+    // Submit
+    submit = new JButton("Cifrar...");
+    panel = new JPanel(new GridLayout(3, 1));
+    panel.add(user_label);
+    panel.add(userName_text);
+    panel.add(password_label);
+    panel.add(password_text);
+    message = new JLabel();
+    panel.add(message);
+    panel.add(submit);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Adding the listeners to components..
+    submit.addActionListener(this);
+    add(panel, BorderLayout.CENTER);
+    setTitle("Please Login Here !");
+    setSize(450, 350);
+    setVisible(true);
+  }
+
+  public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
       new Login();
+      new RSAKeyPairGenerator().printKeysInTerminal();
    }
    @Override
    public void actionPerformed(ActionEvent ae) {
@@ -50,6 +54,4 @@ public class Login extends JFrame implements ActionListener {
          message.setText(" Invalid user.. ");
       }
    }
-
-   
 }
---
+++ RSAKeyPairGenerator.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.security.*;
+import java.util.Base64;
+
+public class RSAKeyPairGenerator {
+
+  private PrivateKey privateKey;
+  private PublicKey publicKey;
+
+  public RSAKeyPairGenerator() throws NoSuchAlgorithmException {
+      KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+      keyGen.initialize(1024);
+      KeyPair pair = keyGen.generateKeyPair();
+      this.privateKey = pair.getPrivate();
+      this.publicKey = pair.getPublic();
+  }
+
+  // public void writeToFile(String path, byte[] key) throws IOException {
+  //     File f = new File(path);
+  //     f.getParentFile().mkdirs();
+
+  //     FileOutputStream fos = new FileOutputStream(f);
+  //     fos.write(key);
+  //     fos.flush();
+  //     fos.close();
+  // }
+
+  public PrivateKey getPrivateKey() {
+      return privateKey;
+  }
+
+  public PublicKey getPublicKey() {
+      return publicKey;
+  }
+  
+  public void printKeysInTerminal() throws NoSuchAlgorithmException, IOException {
+      System.out.println("###########################");
+      System.out.println(Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+      System.out.println("###########################");
+      System.out.println(Base64.getEncoder().encodeToString(privateKey.getEncoded()));
+  }
+}
